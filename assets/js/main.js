b2993f96e78b4575840169e773a7c8688ce65f66
cuchipu <cuchipu25@yopmail.com>
2016-10-30 21:14:10 +0100

    Delete main.js

--- assets/js/main.js
+++
@@ -1,9 +0,0 @@
-var heartuptimer = setInterval(heartup, 200);
-function heartup() {
-  $(".heart").css("font-weight", "900");
-}
-
-var heartdowntimer = setInterval(heartdown, 500);
-function heartdown() {
-  $(".heart").css("font-weight", "100");
-}
